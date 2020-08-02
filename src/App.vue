3f033bc80bef3325de338bf67ba6c27039ea7cc8
sean <1873936160@qq.com>
2020-08-02 15:48:54 +0800

    初始化路由

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+   <div>导航 |<router-link to="/">Frank </router-link>|<router-link to="/xxx">Frank2</router-link></div><hr/>
+   <router-view/>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+ 
+}
+</script>
