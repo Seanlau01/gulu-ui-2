674eb02df97992495a29ec79c34726c4e2d1e48f
sean <1873936160@qq.com>
2020-08-02 20:45:03 +0800

    完善home样式并初始化aside的html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <Topnav/>
-    <div class="banner">
+      <div class="banner">
         <h1>咕噜UI</h1>
         <h2>一个厉害的UI框架</h2>
         <p class="actions">
-           <a>GitHub</a>
-           <a>开始</a>
+           <a href="https://github.com">GitHub</a>
+           <router-link to="/doc">开始</router-link>
         </p>
-    </div>
+      </div>
     </div>
 </template>
 
@@ -20,6 +20,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.banner{
+   background:#89DF8D;
+   padding:100px 0;
+   display:flex;
+   justify-content: center;
+   align-items:center;
+   flex-direction:column;
+   .actions{
+     padding:8px 0; 
+   }
+    a{
+        background:white;
+        border-radius:8px;
+        padding:3px 5px;
+        margin:0 8px;
+        display:inline-block;
+        $h:28px;
+        height:$h;
+        line-height:$h;
+        border-radius:$h/2;
+        padding:0 8px;
+    }
+}
 </style>
     
