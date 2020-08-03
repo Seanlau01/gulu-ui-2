88b902ec4e1c93afa67778cc686a0fbbf8bae97b
sean <1873936160@qq.com>
2020-08-03 16:49:01 +0800

    Topnav float布局改为flex布局

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,12 +1,10 @@
 <template>
     <div class="topnav clearfix">
         <div class="logo" @click="toggleMenu">LOGO</div>
-          <div class="menu" >
-          <ul >
+          <ul class="menu">
               <li>菜单1</li>
               <li>菜单2</li>
           </ul>
-          </div>
     </div>
 </template>
 <script lang="ts">
@@ -22,24 +20,24 @@ import { inject, Ref } from 'vue'
 <style lang="scss" scoped>
     .topnav{
         background:#FDC7CB;
-        padding:20px;  
+        padding:16px;  
         position:relative;
         z-index:10;
+        display:flex;
+        .logo{
+            max-width:6em;
+          margin-right:auto;
+            }
+        .menu{
+            display: flex;
+            flex-wrap:nowrap;
+            white-space: nowrap;
+            li{
+              margin:0 1em;
+         }
     }
-    .clearfix::after{
-        content:'';
-        clear:both;
-        display:block;
+    
     }
-    .logo{
-        float:left;
-    }
-    ul{
-        float:right;
-    }
-    li{
-        float:left;
-        margin-left:30px;
-    }
+    
     
 </style>
