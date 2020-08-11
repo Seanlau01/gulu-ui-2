e4a13196b2ef77b9170e7b1773b8c37051eb7677
Seanlau <lausean561@gmail.com>
2020-08-11 17:29:44 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
-     我是 Button
+   <Button>
+    我是 Button
+   </Button>
     </div>
 </template>
+<script lang="ts">
+import Button from 'Button.vue'
+export default {
+   component:{ Button }
+}
+</script>
