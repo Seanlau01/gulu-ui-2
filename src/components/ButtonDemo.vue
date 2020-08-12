7ac8c8f38889979386b5f7742162e71f0d798884
Seanlau <lausean561@gmail.com>
2020-08-12 10:11:04 +0800

    添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,11 @@
 <template>
+    <div>Button 示例</div>
+    <h1>示例1</h1>
     <div>
-   <Button @click="onClick"
-   @focus="onClick"
-   @mouseover="onClick">
-    我是 Button
-   </Button>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
     </div>
 </template>
 <script lang="ts">
@@ -13,7 +14,7 @@ export default {
    components:{ Button },
    setup(){
       const onClick=()=>{
-
+        console.log('hi')
       }
       return {onClick}
    }
